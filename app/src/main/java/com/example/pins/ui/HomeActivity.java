9d969e8b570c29e71db315c121fac447c37e6de7
jagmeet-singh2091051 <64850872+jagmeet-singh2091051@users.noreply.github.com>
2021-07-09 22:34:42 -0400

    profile page update

--- app/src/main/java/com/example/pins/ui/HomeActivity.java
+++ app/src/main/java/com/example/pins/ui/HomeActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.pins.R;
@@ -22,9 +23,9 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         //BottomNavigationView navView = findViewById(R.id.nav_view);
-        /*AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_myboard, R.id.navigation_teamboard, R.id.navigation_profile)
-                .build();*/
+                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_home);
         //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
--- app/src/main/java/com/example/pins/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/pins/ui/profile/ProfileFragment.java
@@ -12,27 +12,70 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.pins.R;
 import com.example.pins.databinding.FragmentProfileBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import org.jetbrains.annotations.NotNull;
 
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
+ private    TextView name_i,email_i;
 
+      FirebaseDatabase database;
+ DatabaseReference reference;
+FirebaseAuth firebaseAuth;
+FirebaseUser user;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        textView.setText("This is Profile Fragment");
 
-        return root;
+
+
+
+        name_i=root.findViewById(R.id.profile_email);
+email_i=root.findViewById(R.id.profile_email);
+        firebaseAuth=FirebaseAuth.getInstance();
+        user=FirebaseAuth.getInstance().getCurrentUser();
+        database=FirebaseDatabase.getInstance();
+        reference=database.getReference("Users");
+        Query query=reference.orderByChild("email").equalTo(user.getEmail());
+query.addValueEventListener(new ValueEventListener() {
+    @Override
+    public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+
+        for(DataSnapshot ds : snapshot.getChildren()){
+            String name=""+ ds.child("firstname").getValue();
+            String email=""+ ds.child("email").getValue();
+name_i.setText(name);
+email_i.setText(email);
+
+
+        }
+
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    public void onCancelled(@NonNull @NotNull DatabaseError error) {
+
     }
-}
+});
+
+        return root;
+
+    }
+
+    }
+
