82253c0e17b7701640f02d9f32e0e1915e9abab0
Amanpreet Singh <36414846+AmanGujral@users.noreply.github.com>
2021-07-25 12:27:04 -0400

    Task Interaction (Change status)

--- app/src/main/java/com/example/pins/ui/my_board/MyBoardFragment.java
+++ app/src/main/java/com/example/pins/ui/my_board/MyBoardFragment.java
@@ -1,5 +1,6 @@
 package com.example.pins.ui.my_board;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -8,6 +9,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
@@ -15,6 +19,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -25,6 +30,7 @@ import com.example.pins.databinding.FragmentMyboardBinding;
 import com.example.pins.models.ProjectModel;
 import com.example.pins.models.TaskModel;
 import com.example.pins.models.UserModel;
+import com.example.pins.structures.NameAdapter;
 import com.example.pins.structures.TaskAdapter;
 import com.example.pins.ui.project_search.ProjectSearchActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -34,11 +40,13 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.SetOptions;
 
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class MyBoardFragment extends Fragment implements TaskAdapter.ItemClickListener {
 
@@ -82,6 +90,11 @@ public class MyBoardFragment extends Fragment implements TaskAdapter.ItemClickLi
     List<TaskModel> doingTaskList = new ArrayList<>();
     List<TaskModel> doneTaskList = new ArrayList<>();
     String query = "";
+    String currentTaskStatus;
+
+    Boolean isTodoBoardActive = true;
+    Boolean isDoingBoardActive = false;
+    Boolean isDoneBoardActive = false;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -180,7 +193,7 @@ public class MyBoardFragment extends Fragment implements TaskAdapter.ItemClickLi
         joinNowBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getActivity().getApplicationContext(), ProjectSearchActivity.class));
+                startActivity(new Intent(requireContext(), ProjectSearchActivity.class));
             }
         });
 
@@ -262,9 +275,9 @@ public class MyBoardFragment extends Fragment implements TaskAdapter.ItemClickLi
                                 else
                                     todoTaskList.add(taskModel);
                             }
-                            todoRecyclerview.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
-                            doingRecyclerview.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
-                            doneRecyclerview.setLayoutManager(new LinearLayoutManager(getActivity().getApplicationContext()));
+                            todoRecyclerview.setLayoutManager(new LinearLayoutManager(requireContext()));
+                            doingRecyclerview.setLayoutManager(new LinearLayoutManager(requireContext()));
+                            doneRecyclerview.setLayoutManager(new LinearLayoutManager(requireContext()));
 
                             todoTaskAdapter = new TaskAdapter(getContext(), todoTaskList, MyBoardFragment.this);
                             doingTaskAdapter = new TaskAdapter(getContext(), doingTaskList, MyBoardFragment.this);
@@ -317,6 +330,10 @@ public class MyBoardFragment extends Fragment implements TaskAdapter.ItemClickLi
 
         doneCompressed.setVisibility(View.VISIBLE);
         doneExpanded.setVisibility(View.GONE);
+
+        isTodoBoardActive = true;
+        isDoingBoardActive = false;
+        isDoneBoardActive = false;
     }
 
     public void showDoingBoard() {
@@ -328,6 +345,10 @@ public class MyBoardFragment extends Fragment implements TaskAdapter.ItemClickLi
 
         doneCompressed.setVisibility(View.VISIBLE);
         doneExpanded.setVisibility(View.GONE);
+
+        isTodoBoardActive = false;
+        isDoingBoardActive = true;
+        isDoneBoardActive = false;
     }
 
     public void showDoneBoard() {
@@ -339,10 +360,183 @@ public class MyBoardFragment extends Fragment implements TaskAdapter.ItemClickLi
 
         doneCompressed.setVisibility(View.GONE);
         doneExpanded.setVisibility(View.VISIBLE);
+
+        isTodoBoardActive = false;
+        isDoingBoardActive = false;
+        isDoneBoardActive = true;
+    }
+
+    public void showDialog(TaskModel task) {
+        List<String> nameList = new ArrayList<String>();
+        nameList.add("Amanpreet Singh");
+        nameList.add("Ripudaman Singh");
+        nameList.add("Jagmeet Singh");
+
+        // Get current task status
+        currentTaskStatus = task.getStatus();
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(requireActivity());
+        AlertDialog alertDialog;
+
+        LayoutInflater inflater = this.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.alert_dialog_task_details, null);
+        alertDialogBuilder.setView(dialogView);
+
+        TextView dialogTaskDescription = dialogView.findViewById(R.id.alert_dialog_task_description_tv);
+        TextView dialogPriorityTV = dialogView.findViewById(R.id.alert_dialog_task_details_priority_tv);
+        TextView dialogTodoBtn = dialogView.findViewById(R.id.alert_dialog_task_details_todo_btn);
+        TextView dialogDoingBtn = dialogView.findViewById(R.id.alert_dialog_task_details_doing_btn);
+        TextView dialogDoneBtn = dialogView.findViewById(R.id.alert_dialog_task_details_done_btn);
+        RecyclerView dialogRV = dialogView.findViewById(R.id.alert_dialog_task_details_names_rv);
+
+
+        // Set task name/description
+        dialogTaskDescription.setText(task.getTaskName());
+
+        // Set task priority
+        if(task.getPriority().equals(TaskModel.PRIORITY_HIGH)) {
+            dialogPriorityTV.setTextColor(requireActivity().getResources().getColor(R.color.red));
+            dialogPriorityTV.setText("High");
+        }
+        else if (task.getPriority().equals(TaskModel.PRIORITY_MEDIUM)) {
+            dialogPriorityTV.setTextColor(requireActivity().getResources().getColor(R.color.yellow));
+            dialogPriorityTV.setText("Medium");
+        }
+        else {
+            dialogPriorityTV.setTextColor(requireActivity().getResources().getColor(R.color.green));
+            dialogPriorityTV.setText("Low");
+        }
+
+        // Set names list
+        dialogRV.setLayoutManager(new LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false));
+        NameAdapter nameAdapter = new NameAdapter(requireContext(), nameList);
+        dialogRV.setAdapter(nameAdapter);
+
+        // Set status
+        if(currentTaskStatus.equals(TaskModel.STATUS_TODO)) {
+            dialogTodoBtn.setBackgroundColor(requireActivity().getResources().getColor(R.color.green));
+            dialogDoingBtn.setBackgroundColor(requireActivity().getResources().getColor(R.color.white));
+            dialogDoneBtn.setBackgroundColor(requireActivity().getResources().getColor(R.color.white));
+
+            dialogTodoBtn.setTextColor(requireActivity().getResources().getColor(R.color.white));
+            dialogDoingBtn.setTextColor(requireActivity().getResources().getColor(R.color.dark_grey));
+            dialogDoneBtn.setTextColor(requireActivity().getResources().getColor(R.color.dark_grey));
+        }
+        else if(currentTaskStatus.equals(TaskModel.STATUS_DOING)) {
+            dialogTodoBtn.setBackgroundColor(requireActivity().getResources().getColor(R.color.white));
+            dialogDoingBtn.setBackgroundColor(requireActivity().getResources().getColor(R.color.green));
+            dialogDoneBtn.setBackgroundColor(requireActivity().getResources().getColor(R.color.white));
+
+            dialogTodoBtn.setTextColor(requireActivity().getResources().getColor(R.color.dark_grey));
+            dialogDoingBtn.setTextColor(requireActivity().getResources().getColor(R.color.white));
+            dialogDoneBtn.setTextColor(requireActivity().getResources().getColor(R.color.dark_grey));
+        }
+        else {
+            dialogTodoBtn.setBackgroundColor(requireActivity().getResources().getColor(R.color.white));
+            dialogDoingBtn.setBackgroundColor(requireActivity().getResources().getColor(R.color.white));
+            dialogDoneBtn.setBackgroundColor(requireActivity().getResources().getColor(R.color.green));
+
+            dialogTodoBtn.setTextColor(requireActivity().getResources().getColor(R.color.dark_grey));
+            dialogDoingBtn.setTextColor(requireActivity().getResources().getColor(R.color.dark_grey));
+            dialogDoneBtn.setTextColor(requireActivity().getResources().getColor(R.color.white));
+        }
+
+        alertDialog = alertDialogBuilder.create();
+        alertDialog.setCancelable(true);
+        alertDialog.show();
+
+        dialogTodoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogTodoBtn.setBackgroundColor(requireActivity().getResources().getColor(R.color.green));
+                dialogDoingBtn.setBackgroundColor(requireActivity().getResources().getColor(R.color.white));
+                dialogDoneBtn.setBackgroundColor(requireActivity().getResources().getColor(R.color.white));
+
+                dialogTodoBtn.setTextColor(requireActivity().getResources().getColor(R.color.white));
+                dialogDoingBtn.setTextColor(requireActivity().getResources().getColor(R.color.dark_grey));
+                dialogDoneBtn.setTextColor(requireActivity().getResources().getColor(R.color.dark_grey));
+
+                currentTaskStatus = TaskModel.STATUS_TODO;
+
+                if(!currentTaskStatus.equals(task.getStatus())) {
+                    updateTaskDetails(task);
+                    alertDialog.dismiss();
+                }
+            }
+        });
+
+        dialogDoingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogTodoBtn.setBackgroundColor(requireActivity().getResources().getColor(R.color.white));
+                dialogDoingBtn.setBackgroundColor(requireActivity().getResources().getColor(R.color.green));
+                dialogDoneBtn.setBackgroundColor(requireActivity().getResources().getColor(R.color.white));
+
+                dialogTodoBtn.setTextColor(requireActivity().getResources().getColor(R.color.dark_grey));
+                dialogDoingBtn.setTextColor(requireActivity().getResources().getColor(R.color.white));
+                dialogDoneBtn.setTextColor(requireActivity().getResources().getColor(R.color.dark_grey));
+
+                currentTaskStatus = TaskModel.STATUS_DOING;
+
+                if(!currentTaskStatus.equals(task.getStatus())) {
+                    updateTaskDetails(task);
+                    alertDialog.dismiss();
+                }
+            }
+        });
+
+        dialogDoneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialogTodoBtn.setBackgroundColor(requireActivity().getResources().getColor(R.color.white));
+                dialogDoingBtn.setBackgroundColor(requireActivity().getResources().getColor(R.color.white));
+                dialogDoneBtn.setBackgroundColor(requireActivity().getResources().getColor(R.color.green));
+
+                dialogTodoBtn.setTextColor(requireActivity().getResources().getColor(R.color.dark_grey));
+                dialogDoingBtn.setTextColor(requireActivity().getResources().getColor(R.color.dark_grey));
+                dialogDoneBtn.setTextColor(requireActivity().getResources().getColor(R.color.white));
+
+                currentTaskStatus = TaskModel.STATUS_DONE;
+
+                if(!currentTaskStatus.equals(task.getStatus())) {
+                    updateTaskDetails(task);
+                    alertDialog.dismiss();
+                }
+            }
+        });
+    }
+
+    void updateTaskDetails(TaskModel task) {
+        task.setStatus(currentTaskStatus);
+        FirebaseFirestore.getInstance()
+                .collection("Projects")
+                .document(currentProject.getProjectId())
+                .collection("Tasks")
+                .document(task.getTaskId())
+                .set(task, SetOptions.merge())
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull @NotNull Task<Void> task) {
+                        if(task.isSuccessful()) {
+                            getTasks();
+                        }
+                    }
+                });
     }
 
     @Override
     public void onItemClick(View view, int position) {
-        Log.e("Task Name", todoTaskList.get(position).getTaskName());
+        if(isTodoBoardActive) {
+            Log.e("Task Name", todoTaskList.get(position).getTaskName());
+            showDialog(todoTaskList.get(position));
+        }
+        if(isDoingBoardActive) {
+            Log.e("Task Name", doingTaskList.get(position).getTaskName());
+            showDialog(doingTaskList.get(position));
+        }
+        if(isDoneBoardActive) {
+            Log.e("Task Name", doneTaskList.get(position).getTaskName());
+            showDialog(doneTaskList.get(position));
+        }
     }
 }
